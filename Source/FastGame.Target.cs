22a7f4c31f5ef347e9fe50c85eee3f012010ad6f
Krot9ira <90332757+Krot9ira@users.noreply.github.com>
2022-05-13 17:33:10 +0300

    Initial commit

---
+++ Source/FastGame.Target.cs
@@ -0,0 +1,14 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class FastGameTarget : TargetRules
+{
+	public FastGameTarget( TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+		ExtraModuleNames.AddRange( new string[] { "FastGame" } );
+	}
+}
---
+++ Source/FastGame/FastGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FastGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FASTGAME_API AFastGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
